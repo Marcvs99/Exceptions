dda1667b4e483171d703fead6c241fa9db5aad60
Marcus King <marcvs99@gmail.com>
2022-04-29 18:30:04 -0400

    Add project files.

---
+++ Exceptions/Main.cpp
@@ -0,0 +1,100 @@
+// Marcus King
+// 4/29/2022
+// CIS 1202.501
+
+#include <iostream>
+#include <cctype>
+
+class InvalidCharacterException
+{
+};
+
+
+class InvalidRangeException
+{
+};
+
+char character(char start, int offset);
+
+int main()
+{
+	
+	try
+	{
+		std::cout << character('a', 1) << '\n';
+	}
+	catch (InvalidRangeException)
+	{
+		std::cout << "ERROR: InvalidRangeException" << '\n';
+	}
+	catch (InvalidCharacterException)
+	{
+		std::cout << "ERROR: InvalidCharacterException" << '\n';
+	}
+
+	try
+	{
+		std::cout << character('a', -1) << '\n';
+	}
+	catch (InvalidRangeException)
+	{
+		std::cout << "ERROR: InvalidRangeException" << '\n';
+	}
+	catch (InvalidCharacterException)
+	{
+		std::cout << "ERROR: InvalidCharacterException" << '\n';
+	}
+
+	try
+	{
+		std::cout << character('Z', -1) << '\n';
+	}
+	catch (InvalidRangeException)
+	{
+		std::cout << "ERROR: InvalidRangeException" << '\n';
+	}
+	catch (InvalidCharacterException)
+	{
+		std::cout << "ERROR: InvalidCharacterException" << '\n';
+	}
+
+	try
+	{
+		std::cout << character('?', 5) << '\n';
+	}
+	catch (InvalidRangeException)
+	{
+		std::cout << "ERROR: InvalidRangeException" << '\n';
+	}
+	catch (InvalidCharacterException)
+	{
+		std::cout << "ERROR: InvalidCharacterException" << '\n';
+	}
+		
+	
+	
+}
+
+char character(char start, int offset)
+{
+	
+		if ((start < 123 && start > 96) || (start < 91 && start > 64))
+		{
+			int target = start + offset;
+			if ((target < 123 && target > 96) || (target < 91 && target > 64))
+			{
+				return static_cast<char>(static_cast<int>(start) + offset);
+			}
+			else
+			{
+				throw InvalidRangeException();
+			}
+		}
+		else
+		{
+			throw InvalidCharacterException();
+		}
+	
+	
+	
+}
